ec71b63969cd0b7574043b7976e2a25be64609c8
Ponquesitoo <130908036+Ponquesitoo@users.noreply.github.com>
2023-08-25 09:02:29 -0400

    yolo

---
+++ Programa1/prueba_archivo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+int contador = 0;
+
+for(int i = 1; i <= 13; i++) {
+
+    cout << contador << endl;
+    contador++;
+
+}
+
+    return 0;
+}
